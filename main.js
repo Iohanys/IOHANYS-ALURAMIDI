2b5057471eb2d8c364b45772c4f07052f1ed5932
Iohanys <134975916+Iohanys@users.noreply.github.com>
2023-09-25 20:07:24 -0300

    ii

--- main.js
+++ main.js
@@ -11,12 +11,6 @@ function tocaSomTim () {
 }
 document.querySelector('.tecla_tim').onclick = tocaSomTim
 
-function tocaSomClap () {
-   document.querySelector('#som_tecla_clap').play();
-  
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap
-
 
 function tocaSomPuff () {
    document.querySelector('#som_tecla_puff').play();
@@ -25,11 +19,26 @@ function tocaSomPuff () {
 document.querySelector('.tecla_puff').onclick = tocaSomPuff
 
 
+function tocaSomClap () {
+   document.querySelector('#som_tecla_clap').play();
+  
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap
+
+
+
 function tocaSomSplash () {
    document.querySelector('#som_tecla_splash').play();
   
 }
 document.querySelector('.tecla_splash').onclick = tocaSomSplash
+
+
+function tocaSomPuff () {
+   document.querySelector('#som_tecla_puff').play();
+  
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff
 
 
 function tocaSomPsh () {
