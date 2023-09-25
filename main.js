2d86fb0fe6358a0da403d2df5e6d23c1ec6d832b
Iohanys <134975916+Iohanys@users.noreply.github.com>
2023-09-25 19:55:14 -0300

    thy6t

--- main.js
+++ main.js
@@ -5,9 +5,21 @@ function tocaSomPom () {
 document.querySelector('.tecla_pom').onclick = tocaSomPom
 
 
+function tocaSomTim () {
+   document.querySelector('#som_tecla_tim').play();
+  
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim
+
 function tocaSomClap () {
    document.querySelector('#som_tecla_clap').play();
   
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap
+
+
+function tocaSomPuff () {
+   document.querySelector('#som_tecla_puff').play();
   
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff
